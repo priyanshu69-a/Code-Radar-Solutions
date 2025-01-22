20203a19fb7b583368c87b9126381ec4be999add
priyanshu69-a <priyanshurana5002@gmail.com>
2025-01-22 23:36:54 +0530

    Solution to Input and Print a Float

--- Input and Print a Float@@6788d5012db25efec946f93a/code.c
+++ Input and Print a Float@@6788d5012db25efec946f93a/code.c
@@ -5,6 +5,6 @@ int main() {
     float sum;
     printf("enter any value %f :\n");
     scanf("%f",&sum);
-    printf("the value of sum %f is ",sum);
+    printf("the value of sum %f is :",sum);
     return 0;
 }
