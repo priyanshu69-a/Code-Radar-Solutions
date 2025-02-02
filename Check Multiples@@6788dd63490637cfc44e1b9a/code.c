7984ea309f12bd7ca3bb47b8ac78b29cc90a59fb
priyanshu69-a <priyanshurana5002@gmail.com>
2025-02-02 14:52:36 +0530

    Solution to Check Multiples

--- Check Multiples@@6788dd63490637cfc44e1b9a/code.c
+++ Check Multiples@@6788dd63490637cfc44e1b9a/code.c
@@ -2,11 +2,10 @@
 int main() {
     int divisor,num;
     scanf("%d %d",&divisor,&num);
-    if(divisor==0){
-        if(num % divisor == 0){
+  
+    if(num % divisor == 0){
             print("Yes");
         }
-    }
     else{
         printf("No");
     }
