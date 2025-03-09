8c0a115ec30a53ccc08523bfe5650eb27a844103
priyanshu69-a <priyanshurana5002@gmail.com>
2025-03-09 10:43:17 +0530

    Solution to Fibonacci Series

---
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -0,0 +1,14 @@
+int main(){
+    int n;
+    scanf("%d",&n);
+    int fib[n];
+    int fib[0]=0;
+    int fib[1]=1;
+    for(int i=2;i<n;i++){
+        fib[i]=fib[i-1]+fib[i-2];
+        printf("%d \t",fib[i]);
+    }
+    printf("\n");
+    return 0; 
+
+}
